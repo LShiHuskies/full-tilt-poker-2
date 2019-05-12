b219fd9ea5a6eeefc3e3e114e098d106d7e00367
Louis Shi <Shilou2010@gmail.com>
2019-05-11 23:27:45 -0400

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Formik } from "formik";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { Card, CardContent } from '@material-ui/core/';
 
+import LoginForm from './LoginForm';
 
 const Login = () => {
+  const handleSubmit = (values) => {
+    console.log(handleSubmit)
+  }
   return (
     <div>
-        hello
+      <Card>
+        Welcome to Full-Tilt Poker 2.0
+      </Card>
+      <Formik
+        onSubmit={handleSubmit}
+        render={formikProps => (
+        <LoginForm
+          {...formikProps}
+        />
+        )}
+      />
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Field } from 'formik';
+
+const LoginForm = () => {
+  return (
+    <form>
+        hello
+    </form>
+  )
+};
+
+export default LoginForm;
