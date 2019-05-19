ad27b0170dc39c18960baaf178007798c7b5edd7
Louis Shi <Shilou2010@gmail.com>
2019-05-19 00:45:01 -0400

    login form schema and form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { Card, CardContent } from '@material-ui/core/';
 
+import LoginForm from './LoginForm';
+
 const loginScehema = Yup.object().shape({
     userName: Yup
       .string()
@@ -13,23 +15,32 @@ const loginScehema = Yup.object().shape({
       .min(6, "Too Short"),
   });
 
-  const Login = () => {
+  const Login = ({ handleSubmit }) => {
     return (
       <div>
-      <Card style={{ width: '30%', margin: '1% auto', backgroundColor: 'lightblue' }}>
-        <CardContent>
-          Welcome Back!
-        </CardContent>
-      </Card>
-      <div style={{fontSize: '14px', padding: '7px',
-           color: 'black', width: '300px', fontHeight: 'normal', fontStretch: 'normal',
-          lineHeight: 'normal', letterSpacing: '-0.2px', textAlign: 'right', position: 'absolute',
-          float: 'left', top: '1.5%', display: 'inline', right: '0.5%'}}>
-          <div style={{display: 'inline', position: 'absolute', top: '100%', left: '0%', right: '35%', color: 'white'}}>Need to Create an Account?</div>
-            <button id='SIGNUP' type="submit" onClick={'handleSignUp'} style={{display: 'inline',
-                color: 'white',
-               border: 'solid, 1px, white', fontWeight: 'bold', float: 'right' }}> Sign Up</button>
-      </div>
+        <Card style={{ width: '30%', margin: '1% auto', backgroundColor: 'lightblue' }}>
+            <CardContent>
+            Welcome Back!
+            </CardContent>
+        </Card>
+        <div style={{fontSize: '14px', padding: '7px',
+            color: 'black', width: '300px', fontHeight: 'normal', fontStretch: 'normal',
+            lineHeight: 'normal', letterSpacing: '-0.2px', textAlign: 'right', position: 'absolute',
+            float: 'left', top: '1.5%', display: 'inline', right: '0.5%'}}>
+            <div style={{display: 'inline', position: 'absolute', top: '100%', left: '0%', right: '35%', color: 'white'}}>Need to Create an Account?</div>
+                <button id='SIGNUP' type="submit" onClick={'handleSignUp'} style={{display: 'inline',
+                    color: 'white',
+                border: 'solid, 1px, white', fontWeight: 'bold', float: 'right' }}> Sign Up</button>
+        </div>
+        <Formik
+          onSubmit={handleSubmit}
+          validationSchema={loginScehema}
+          render={formikProps => (
+          <LoginForm
+          {...formikProps}
+          />
+        )}
+      />
       </div>
     )
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { TextField, Button } from '@material-ui/core/';
+
+const LoginForm = ({
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors,
+    touched,
+    isValid,
+  }) => {
+    return (
+      <form onSubmit={handleSubmit} style={{ width: '50%', margin: '5% auto', backgroundColor: 'lightgray' }}>
+      <br />
+        <h4 style={{ width: '30%', margin: '1% auto', backgroundColor: 'lightgray', border: 'none' }}>
+            Create New Account
+        </h4>
+        <TextField
+          name="userName"
+          label="User Name"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.userName && errors.userName ? errors.userName : null}
+          error={touched.userName && errors.userName ? true : false}
+          fullWidth
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.password && errors.password ? errors.password : null}
+          error={touched.password && errors.password ? true : false}
+          fullWidth
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disabled={!isValid}
+          style={{ marginTop: '5%', display: 'block', marginLeft: 'auto', marginRight: '0' }}
+        >
+        Submit
+        </Button>
+      </form>
+    )
+  };
+  
+  export default LoginForm;
